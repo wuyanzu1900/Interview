f955bfa920aeb57565ecebff316b5afa27034e4e
zhuming <1109282075@qq.com>
2020-09-21 23:18:05 +0800

    react

---
+++ React/drag/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const start = () =>{
+    
+  }
+  return (
+    <div className="App">
+        <div onDragStart={}>box1</div>
+        <div>box1</div>
+    </div>
+  );
+}
+
+export default App;
